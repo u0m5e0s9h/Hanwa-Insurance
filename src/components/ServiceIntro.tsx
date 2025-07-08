129c8ebdac3856fe0669e6233750cc43e7572a8c
umesh kumar <umeshc0509@gmail.com>
2025-07-09 01:44:53 +0530

    modified Serviceintro for mobile view

--- src/components/ServiceIntro.tsx
+++ src/components/ServiceIntro.tsx
@@ -1,4 +1,3 @@
-
 import { useState } from "react";
 import MembershipFormModal from "./MembershipFormModal";
 
@@ -6,27 +5,28 @@ const ServiceIntro = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
-    <section className="bg-white px-8 pt-2 pb-6">
-      <div className="max-w-[1000px] mx-auto space-y-12">
-        
-        <div className="bg-white border border-gray-200 shadow-md px-6 py-5 flex flex-col md:flex-row items-center justify-between gap-6">
+    <section className="bg-white px-4 pt-2 pb-6">
+      <div className="max-w-[1000px] mx-auto space-y-8">
+        <div className="bg-white border border-gray-200 shadow-md px-4 py-5 flex flex-row items-center justify-between gap-4 flex-wrap md:flex-nowrap">
           
-          {/* Image block */}
-          <div className="flex-shrink-0">
+          {/* Image */}
+          <div className="flex-1 min-w-[150px] max-w-[70%]">
             <img
               src="/service.png"
               alt="행복 신청 이미지"
-              className="w-[636px] h-[114.5px]  object-contain"
+              className="w-full h-auto object-contain"
             />
           </div>
 
-          {/* Button only */}
-          <button
-            onClick={() => setIsModalOpen(true)}
-            className="w-[264px] h-[74px] text-sm font-semibold text-gray-900 border border-orange-500 rounded-full px-[32px] py-[24px] hover:bg-orange-50 transition text-center"
-          >
-            “나눔의 행복”<br />가입 신청하기
-          </button>
+          {/* Button  */}
+          <div className="flex-1 min-w-[150px] max-w-[30%]">
+            <button
+              onClick={() => setIsModalOpen(true)}
+              className="w-full text-sm font-semibold text-gray-900 border border-orange-500 rounded-full px-4 py-4 hover:bg-orange-50 transition text-center"
+            >
+              “나눔의 행복”<br />가입 신청하기
+            </button>
+          </div>
         </div>
       </div>
 
